c0f2e796a8d60a1f62d6584fd352fe5c2e8b080e
Jayakrishna D <djkrishnajk@gmail.com>
2020-07-03 06:52:49 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Login from "./Components/login";
-import Home from "./Components/Home";
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './Components/login';
+import Home from './Components/Home';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
-          <Route exact path="/" component={Login} />
-          <Route path="/Home" component={Home} />
+                <Route exact path="/" component={Login} />
+                <Route path="/home" component={Home} />
         </Switch>
       </Router>
     </div>
   );
 }
 
-export default App;
+export default App
